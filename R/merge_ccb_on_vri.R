bb84fd828c8ff728355279cc9a2a2cc6e84ae094
Nicolas Gauthier <nicolas.gauthier@lacapitale.com>
2022-01-23 18:51:52 -0500

    added placeholder for merge of ccb on vri calc_forest_age_class placeholder for raster merge

---
+++ R/merge_ccb_on_vri.R
@@ -0,0 +1,6 @@
+merge_ccb_on_vri <- function(ccb, vri) {
+
+  # merge ccb on vri
+
+  # not same polygon so need to find out how to merge
+}
---
+++ R/merge_elevation_raster_on_bem.R
@@ -0,0 +1,10 @@
+merge_elevation_raster_on_bem <- function(rstr, bem) {
+  # compute mean of slope of raster's cells in each polygon
+
+  # compute circular mean of aspect  raster's cells in each polygon
+
+  # compute mean of elevation raster's cells in each polygon
+
+  # create indicator base on elevation treshold
+
+}
