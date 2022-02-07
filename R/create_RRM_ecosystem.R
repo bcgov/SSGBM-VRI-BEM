5ff340ae3b563b1711ac89230ac63efe15bd4291
FrankBornais <francois.bornais-doucet@lacapitale.com>
2022-02-06 23:02:42 -0500

    update doc

--- R/create_RRM_ecosystem.R
+++ R/create_RRM_ecosystem.R
@@ -1,5 +1,8 @@
 #' Create RRM Ecosystem information
 #'
+#' This function generates the unique list of ecosystems and determines all possible combinations of STS and STAND codes
+#' that could apply to the ecosystem at any age value, and expand to include all existing and "potential" unique ecosystems
+#'
 #' @param bfc bem feature class
 #'
 #' @return Summary of Area by unique ecosystem
--- R/validate_required_attributes.R
+++ R/validate_required_attributes.R
@@ -1,9 +1,19 @@
+
+#' Validate that Required Attributes are in the object
+#'
+#' @param ifc input feature class
+#' @param required_attributes names of required attributes
+#'
+#' @return error if attribute is missing
+#' @export
+#'
+
 validate_required_attributes <- function(ifc, required_attributes){
 
   missing_attributes <- setdiff(required_attributes,
                                 names(ifc))
 
   if (length(missing_attributes) > 0) {
-    stop("The following attributes were not found in `ifc` : ", paste(missing_attributes, collapse = ", "))
+    stop("The following attributes were not found : ", paste(missing_attributes, collapse = ", "))
   }
 }
