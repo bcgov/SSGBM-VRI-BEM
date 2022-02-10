ece82f65c0ad3e4828addb01df7d932c991c374b
Nicolas Gauthier <nicolas.gauthier@lacapitale.com>
2022-02-09 23:36:24 -0500

    create read layers

---
+++ R/read_layers.R
@@ -0,0 +1,89 @@
+#' Read vri sf object
+#'
+#' read the vegetation ressource inventory layer
+#'
+#' @param dsn data source name (interpretation varies by driver - for some drivers, dsn is a file name, but may also be a folder, or contain the name and access credentials of a database); in case of GeoJSON, dsn may be the character string holding the geojson data. It can also be an open database connection.
+#' @param layer layer name (varies by driver, may be a file name without extension); in case layer is missing, st_read will read the first layer of dsn, give a warning and (unless quiet = TRUE) print a message when there are multiple layers, or give an error if there are no layers in dsn. If dsn is a database connection, then layer can be a table name or a database identifier (see Id). It is also possible to omit layer and rather use the query argument.
+#' @return sf object
+#' @import sf
+#' @export
+read_vri <- function(dsn, layer = "VEG_R1_PLY_polygon") {
+  vri <- st_read(dsn = dsn, layer = layer, quiet = TRUE)
+  #Restructure bem while waiting for real info
+  vri <- rename_geometry(vri, "Shape")
+  #make shape valid because ARCGIS draw polygon differently than sf
+  vri$Shape <- st_cast(st_make_valid(vri$Shape),"MULTIPOLYGON")
+  return(vri)
+}
+
+#' Read bem sf object
+#'
+#' read the broad ecosytem mapping layer
+#'
+#' @param dsn data source name (interpretation varies by driver - for some drivers, dsn is a file name, but may also be a folder, or contain the name and access credentials of a database); in case of GeoJSON, dsn may be the character string holding the geojson data. It can also be an open database connection.
+#' @param layer layer name (varies by driver, may be a file name without extension); in case layer is missing, st_read will read the first layer of dsn, give a warning and (unless quiet = TRUE) print a message when there are multiple layers, or give an error if there are no layers in dsn. If dsn is a database connection, then layer can be a table name or a database identifier (see Id). It is also possible to omit layer and rather use the query argument.
+#' @return sf object
+#' @import sf
+#' @export
+read_bem <- function(dsn, layer = "BEM") {
+  bem <- st_read(dsn = dsn, layer = layer, quiet = TRUE)
+  #Restructure bem while waiting for real info
+  bem <- rename_geometry(bem, "Shape")
+  #make shape valid because ARCGIS draw polygon differently than sf
+  bem$Shape <- sf::st_make_valid(bem$Shape)
+  return(bem)
+}
+
+
+
+#' Read wetlands sf object
+#'
+#' read the wetlands layer
+#'
+#' @param dsn data source name (interpretation varies by driver - for some drivers, dsn is a file name, but may also be a folder, or contain the name and access credentials of a database); in case of GeoJSON, dsn may be the character string holding the geojson data. It can also be an open database connection.
+#' @param layer layer name (varies by driver, may be a file name without extension); in case layer is missing, st_read will read the first layer of dsn, give a warning and (unless quiet = TRUE) print a message when there are multiple layers, or give an error if there are no layers in dsn. If dsn is a database connection, then layer can be a table name or a database identifier (see Id). It is also possible to omit layer and rather use the query argument.
+#' @return sf object
+#' @import sf
+#' @export
+read_wetlands <- function(dsn, layer = "FWA_WETLANDS_POLY") {
+  wetlands <- st_read(dsn = dsn, layer = layer, quiet = TRUE)
+  #Restructure bem while waiting for real info
+  wetlands <- rename_geometry(wetlands, "Shape")
+  #make shape valid because ARCGIS draw polygon differently than sf
+  wetlands$Shape <- sf::st_make_valid(wetlands$Shape)
+  return(wetlands)
+}
+
+
+#' Read rivers sf object
+#'
+#' read the rivers layer
+#'
+#' @param dsn data source name (interpretation varies by driver - for some drivers, dsn is a file name, but may also be a folder, or contain the name and access credentials of a database); in case of GeoJSON, dsn may be the character string holding the geojson data. It can also be an open database connection.
+#' @param layer layer name (varies by driver, may be a file name without extension); in case layer is missing, st_read will read the first layer of dsn, give a warning and (unless quiet = TRUE) print a message when there are multiple layers, or give an error if there are no layers in dsn. If dsn is a database connection, then layer can be a table name or a database identifier (see Id). It is also possible to omit layer and rather use the query argument.
+#' @return sf object
+#' @import sf
+#' @export
+read_rivers <- function(dsn, layer = "FWA_RIVERS_POLY") {
+  rivers <- st_read(dsn = dsn, layer = layer, quiet = TRUE)
+  #make shape valid because ARCGIS draw polygon differently than sf
+  rivers$GEOMETRY <- sf::st_make_valid(rivers$GEOMETRY)
+  return(rivers)
+}
+
+#' Read ccb sf object
+#'
+#' read the consolidated cutblocks layer
+#'
+#' @param dsn data source name (interpretation varies by driver - for some dccb, dsn is a file name, but may also be a folder, or contain the name and access credentials of a database); in case of GeoJSON, dsn may be the character string holding the geojson data. It can also be an open database connection.
+#' @param layer layer name (varies by driver, may be a file name without extension); in case layer is missing, st_read will read the first layer of dsn, give a warning and (unless quiet = TRUE) print a message when there are multiple layers, or give an error if there are no layers in dsn. If dsn is a database connection, then layer can be a table name or a database identifier (see Id). It is also possible to omit layer and rather use the query argument.
+#' @return sf object
+#' @import sf
+#' @export
+read_ccb <- function(dsn, layer = "CNS_CUT_BL_polygon") {
+  ccb <- st_read(dsn = dsn, layer = layer, quiet = TRUE)
+  #make shape valid because ARCGIS draw polygon differently than sf
+  ccb$Shape <- sf::st_make_valid(ccb$Shape)
+  return(ccb)
+}
+
--- scripts/combine_scripts.R
+++ scripts/combine_scripts.R
@@ -1,19 +1,8 @@
 devtools::load_all()
 
-vri <- sf::st_read(dsn = "../SSGBM-VRI-BEM-data/VEG_COMP_LYR_R1_POLY", layer = "VEG_R1_PLY_polygon", quiet = TRUE)
-bem <- sf::st_read(dsn = "../SSGBM-VRI-BEM-data/BEM_VRI", layer = "BEM", quiet = TRUE)
-
-#Restructure bem while waiting for real info
-bem <- rename_geometry(bem, "Shape")
-
-#Restructure bem while waiting for real info
-vri <- rename_geometry(vri, "Shape")
-
-#make shape valid
-#TODO switch to use geometry
-
-bem$Shape <- sf::st_make_valid(bem$Shape)
-vri$Shape <- st_make_valid(vri$Shape) |> st_cast("MULTIPOLYGON")
+# read vri and bem layers
+vri <- read_vri("../SSGBM-VRI-BEM-data/VEG_COMP_LYR_R1_POLY")
+bem <- read_bem("../SSGBM-VRI-BEM-data/BEM_VRI")
 
 # 1a ----
 vri_bem <- merge_bem_on_vri(vri, bem, return_intersection_dt = TRUE)
@@ -25,51 +14,53 @@ vri_bem <- vri_bem[which(!is.na(vri_bem$TEIS_ID)),]
 
 # 1b ----
 beu_bec_csv <- fread("csv/Allowed_BEC_BEUs_NE_ALL.csv")
-rivers <- sf::st_read(dsn = "../SSGBM-VRI-BEM-data/CodeWithUs.gdb", layer = "FWA_RIVERS_POLY", quiet = TRUE)
-vri_bem_updated <- update_bem_from_vri(ifc = vri_bem,
-                                       rfc = rivers,
-                                       beu_bec = beu_bec_csv,
-                                       clear_site_ma = TRUE)
+rivers <- read_rivers("../SSGBM-VRI-BEM-data/CodeWithUs.gdb")
+
+vri_bem <- update_bem_from_vri(ifc = vri_bem,
+                               rfc = rivers,
+                               beu_bec = beu_bec_csv,
+                               clear_site_ma = TRUE)
 
 #1c ----
 beu_wetland_update_csv <- fread("csv/beu_wetland_updates.csv")
-wetlands <- sf::st_read(dsn = "../SSGBM-VRI-BEM-data/CodeWithUs.gdb", layer = "FWA_WETLANDS_POLY", quiet = TRUE)
-wetlands <- rename_geometry(wetlands, "Shape")
+wetlands <- read_wetlands("../SSGBM-VRI-BEM-data/CodeWithUs.gdb")
 
-updated_bem_from_wetland <- update_bem_from_wet(bfc = vri_bem_updated,
-                                                wfc = wetlands,
-                                                buc = beu_wetland_update_csv)
+vri_bem <- update_bem_from_wet(bfc = vri_bem,
+                               wfc = wetlands,
+                               buc = beu_wetland_update_csv)
 
 #2 ----
-unique_eco <- create_unique_ecosytem_dt(ifc = updated_bem_from_wetland)
+unique_eco <- create_unique_ecosytem_dt(ifc = vri_bem)
 
 fwrite(unique_eco, file = "../unique_ecosystem.csv")
+
 
 #3abc ----
 elev_rast <- terra::rast("../SSGBM-VRI-BEM-data/DEM_tif/dem.tif")
 
-updated_bem_from_wetland <- merge_elevation_raster_on_sf(elev_raster = elev_rast,
-                                    ifc = updated_bem_from_wetland)
+vri_bem <- merge_elevation_raster_on_sf(elev_raster = elev_rast,
+                                        ifc = vri_bem)
 
 # merge cutblock
-ccb <- sf::st_read(dsn = "../SSGBM-VRI-BEM-data/CodeWithUs.gdb", layer = "CNS_CUT_BL_polygon", quiet = TRUE)
+ccb <- read_ccb("../SSGBM-VRI-BEM-data/CodeWithUs.gdb")
 
-updated_bem_from_wetland <- merge_ccb_on_vri(ccb = ccb, vri = updated_bem_from_wetland)
+vri_bem <- merge_ccb_on_vri(vri = vri_bem, ccb = ccb)
 
 #4 ----
-vri_forest_age <- calc_forest_age_class(vri = updated_bem_from_wetland,
-                                        most_recent_harvest_year = 2020)
+vri_bem <- calc_forest_age_class(vri = vri_bem,
+                                 most_recent_harvest_year = 2020)
+
 
 #4b /4d2 ----
 unique_eco_example <- read_unique_ecosystem_dt("csv/Skeena_VRIBEM_LUT.csv")
-vri_all_info <- merge_unique_ecosystem_fields(ifc = vri_forest_age,
-                                              unique_ecosystem_dt = unique_eco_example)
+vri_bem <- merge_unique_ecosystem_fields(ifc = vri_bem,
+                                         unique_ecosystem_dt = unique_eco_example)
 
 #4d3 ----
-vri_all_info <- find_crown_area_dominant_values(vri = vri_all_info,
+vri_bem <- find_crown_area_dominant_values(vri = vri_bem,
                                                 bem = bem,
                                                 intersection_dt = vri_bem_intersection_dt)
 
 #5 ----
-export_dt <- create_RRM_ecosystem(bfc = vri_all_info)
+export_dt <- create_RRM_ecosystem(bfc = vri_bem)
 fwrite(export_dt, file = "../RRM_input_table.csv")
