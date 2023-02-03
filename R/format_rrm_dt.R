bae1a0e942cc2b5804dad34bec9924cdb6a8a834
Waytes <Riley.Waytes@gov.bc.ca>
2023-02-03 10:47:25 -0800

    Fixed format_rrm_dt to include moose variables.

--- R/format_rrm_dt.R
+++ R/format_rrm_dt.R
@@ -6,7 +6,8 @@
 #' @return data.table  formatted rrm_dt
 #' @import data.table
 #' @export
-format_rrm_dt <- function(rrm_dt) {
+format_rrm_dt <- function(rrm_dt,animal) { #added animal because will change based on bear/moose
+  if(animal == "bear") { #added bear to differentiate
   rrm_dt[, Bgc_vrt := as.character(Bgc_vrt)]
   rrm_dt[is.na(Bgc_vrt), Bgc_vrt := ""]
 
@@ -16,6 +17,19 @@ format_rrm_dt <- function(rrm_dt) {
 
   setorder(rrm_dt, Eco_sec, Bgc_zone, Bgc_subzon, Bgc_vrt, Bgc_phase, Beumc,
            Slope_mod, Site_m3a, Snow_code, Above_Elev_Thold, Crown_Bear, MURAR_PEFD_6C)
+
+  }
+  if(animal == "moose") { #all code below is new, to distinguish moose from bear
+    rrm_dt[, Bgc_vrt := as.character(Bgc_vrt)]
+    rrm_dt[is.na(Bgc_vrt), Bgc_vrt := ""]
+
+    rrm_dt[, Bgc_phase := as.character(Bgc_phase)]
+
+    rrm_dt[Site_m3a == "", Site_m3a := NA_character_]
+
+    setorder(rrm_dt, Eco_sec, Bgc_zone, Bgc_subzon, Bgc_vrt, Bgc_phase, Beumc,
+             Slope_mod, Site_m3a, Snow_code, Above_Elev_Thold, Crown_Moose, MALAN_WFD_6C)
+  }
 
   return(rrm_dt)
 }
